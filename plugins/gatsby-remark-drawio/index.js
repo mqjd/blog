0b8ee2d8de1c4e0f41a2e4505a551daf87963471
mqjd <1378415278@qq.com>
2022-01-04 23:06:10 +0800

    update blog

--- plugins/gatsby-remark-drawio/index.js
+++ plugins/gatsby-remark-drawio/index.js
@@ -1,63 +1,24 @@
 const visit = require("unist-util-visit")
-
-const resolveParams = (text) => {
-  var prefixLength= "drawio:".length;
-  if (text.length<= prefixLength) {
-    return;
-  }
-  return text.substr(prefixLength).split(";").map(v => {
-    let result = {};
-    if (v.indexOf("=") !== -1){
-      result[v.substr(0, v.indexOf("="))] = resoveValue(v.substr(v.indexOf("=") + 1));
-    } else {
-      result[v] = true;
-    }
-    return result;
-  }).reduce((prev, cur) => {return {...prev, ...cur}})
-}
-
-const resoveValue = (value) => {
-  if (value.length === 0) {
-    return "";
-  }
-
-  if("null" === value){
-    return null;
-  }
-
-  if(!isNaN(value)){
-    return +value;
-  }
-
-  if("true" === value){
-    return true;
-  }
-
-  if("false" === value){
-    return false;
-  }
-  return value;
-}
+const prefix = "drawio";
+const prefixLength = prefix.length + 1;
 
 module.exports = ({ markdownAST }, options) => {
   visit(markdownAST, "code", node => {
     let lang = (node.lang || "").toLowerCase()
-    if (lang.indexOf("drawio") === 0) {
+    if (lang.indexOf(prefix) === 0) {
       var url = node.value
       if (url.indexOf("/") !== 0 && url.indexOf("http") !== 0) {
         url = options.baseUrl + url
       }
-      let params = resolveParams(lang);
-      let props = {...options, ...params};
-
+      let param = lang.length <= prefixLength ? "" : lang.substr(prefixLength);
       node.type = "jsx"
       node.value = `<DrawioViewer
-                      highlight="${props.highlight}"
-                      lightbox={${props.lightbox}}
-                      nav={${props.lightbox}}
-                      resize={${props.lightbox}}
-                      toolbar="${props.toolbar}"
-                      page={${props.page}}
+                      highlight="${options.highlight}"
+                      lightbox={${options.lightbox}}
+                      nav={${options.lightbox}}
+                      resize={${options.lightbox}}
+                      toolbar="${options.toolbar}"
+                      param="${param}"
                       url="${url}"/>`
     }
   })
--- plugins/gatsby-theme-mine/src/components/drawio-viewer.tsx
+++ plugins/gatsby-theme-mine/src/components/drawio-viewer.tsx
@@ -7,7 +7,46 @@ type Props = {
     nav: boolean,
     resize: boolean,
     toolbar: string,
-    url: string
+    url: string,
+    param: string,
+}
+
+const resolveParams = (text: string) => {
+    if (!text) {
+        return {};
+    }
+    return text.split(";").map(v => {
+        let result = {};
+        if (v.indexOf("=") !== -1) {
+            result[v.substring(0, v.indexOf("="))] = resoveValue(v.substring(v.indexOf("=") + 1));
+        } else {
+            result[v] = true;
+        }
+        return result;
+    }).reduce((prev, cur) => {return {...prev, ...cur}})
+}
+
+const resoveValue = (value: any) => {
+    if (value.length === 0) {
+        return "";
+    }
+
+    if("null" === value){
+        return null;
+    }
+
+    if(!isNaN(value)){
+        return +value;
+    }
+
+    if("true" === value){
+        return true;
+    }
+
+    if("false" === value){
+        return false;
+    }
+    return value;
 }
 
 const style = {
@@ -21,19 +60,14 @@ const DrawioViewer = ( prop: Props) => {
     const context: any = useDeck()
     const defaultProps = {
         toolbar: context.deck ? null : prop.toolbar || null,
-        "auto-fit": true,
-        "auto-origin": true,
-        "allow-zoom-in": false,
-        "allow-zoom-out": false,
-        "toolbar-position": "top",
-        center: true,
-        responsive: true,
+        "auto-fit": true
     }
+    let params = resolveParams(prop.param);
     React.useEffect(() => {
         let win: any = window;
         const element = container.current
         win.GraphViewer.getUrl(prop.url, function (e) {
-            graphView = new win.GraphViewer(element, win.mxUtils.parseXml(e).documentElement, {...prop, ...defaultProps});
+            graphView = new win.GraphViewer(element, win.mxUtils.parseXml(e).documentElement, {...prop, ...defaultProps, ...params});
         });
     }, [])
     return <div style={style}><div style={{height: "100%"}} ref={container}></div></div>
