761f3d939eb0a96d357f6544a2ae89c7f371f9a2
mqjd <1378415278@qq.com>
2022-01-10 10:17:02 +0800

    update blog

--- optimize.sh
+++ optimize.sh
@@ -3,13 +3,12 @@
 # 确保脚本抛出遇到的错误
 set -e
 
-asserts=$(find public -type f|grep -v .html|grep -v .json)
-htmls=$(find public -type f|grep .html)
+asserts=$(find public -type f | grep -v .html | grep -v .json | grep -v .webmanifest)
+htmls=$(find public -type f | grep .html)
 codingPath="https://mqjd.coding.net/p/m/d/blog/git/raw/master/"
 
 for html in $htmls
 do
-    sed -i "s#crossorigin=\"anonymous\"##g" $html
     for assert in $asserts
     do
         sed -i "s#\"${assert:6}\"#\"${codingPath}${assert:7}\"#g" $html
