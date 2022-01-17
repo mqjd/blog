43f9df40c73a351f8dd0061bb273e8643095b996
mqjd <1378415278@qq.com>
2022-01-17 09:56:57 +0800

    update blog

--- deploy.sh
+++ deploy.sh
@@ -3,29 +3,39 @@
 # 确保脚本抛出遇到的错误
 set -e
 
+git config --global user.name "mqjd"
+git config --global user.email "1378415278@qq.com"
+
+githubUrl=https://mqjd:${GITHUB_TOKEN}@github.com/mqjd/mqjd.github.io.git
+giteeUrl=https://MQJD:${GITEE_TOKEN}@gitee.com/MQJD/mqjd.git master
+# codingUrl=htps://JshEHrjrhN:${CODING_TOKEN}@mqjd.coding.net/mqjd/m/blog.git
+
 npm install -legacy-peer-deps
 npm install --force
 
-# 生成静态文件
+# 部署github
 npm run build
 
 # bash ./optimize.sh
 
-# 进入生成的文件夹
 cd public
-
-# deploy to github
-githubUrl=https://mqjd:${GITHUB_TOKEN}@github.com/mqjd/mqjd.github.io.git
-# codingUrl=htps://JshEHrjrhN:${CODING_TOKEN}@mqjd.coding.net/mqjd/m/blog.git
-
-git config --global user.name "mqjd"
-git config --global user.email "1378415278@qq.com"
-
 git init
 git add -A
 git commit -m "来自github actions的自动部署"
 git push -f $githubUrl master # 推送到github
-# git push -f $codingUrl master # 推送到coding
+
+
+# 部署gitee
+cd ..
+sed -i "s#cdn.jsdelivr.net/gh/mqjd/assets@latest#mqjd.gitee.io/assets#g" gatsby-config.js
+npm run build
+
+cd public
+git init
+git add -A
+git commit -m "来自github actions的自动部署"
+git push -f $giteeUrl master # 推送到gitee
+
 
 cd - # 退回开始所在目录
 rm -rf public
